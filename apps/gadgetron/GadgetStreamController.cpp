0ad9cf78d8b6d3d0491e6e2a88d6b2e693b038d2
marcsous <17042435+marcsous@users.noreply.github.com>
2020-08-02 18:42:05 -0700

    Update GadgetStreamController.cpp

--- apps/gadgetron/GadgetStreamController.cpp
+++ apps/gadgetron/GadgetStreamController.cpp
@@ -197,8 +197,8 @@ int GadgetStreamController::handle_close (ACE_HANDLE, ACE_Reactor_Mask mask)
 
 int GadgetStreamController::configure_from_file(std::string filename)
 {
-  boost::filesystem::path full_path = gadgetron_home_ / GADGETRON_CONFIG_PATH / filename;
-  //boost::filesystem::path full_path = gadgetron_home_ / "recon" / filename; // MB move it somewhere convenient
+  //boost::filesystem::path full_path = gadgetron_home_ / GADGETRON_CONFIG_PATH / filename;
+  boost::filesystem::path full_path = gadgetron_home_ / GADGETRON_CONFIG_PATH / filename / "recon"; // MB move it somewhere convenient
 	
   GINFO("Running configuration: %s\n", full_path.c_str());
 
